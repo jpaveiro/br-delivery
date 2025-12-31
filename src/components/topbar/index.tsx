6d2c9adcfbea6370be4c57b4dcdb8f766451036b
jpaveiro <jpaveiros@gmail.com>
2025-12-31 20:56:30 -0300

    docs: TODO

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
-
 import { styles } from "./styles";
 
 export default function TopBar() {
-    const address = 'Av. Brig. Faria Lima, xxxx'
+    /* TODO -> Pegar o endereço via consulta do endereço padrão ao usuário do jwt. */
+    const address = 'Rua Camarão de Marte, 1234';
+    
     return (
         <View style={styles.wrapper}>
             <Text style={styles.address}>{address}</Text>
