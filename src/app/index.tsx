f6210264dc37c6515f3515c80b28d86f376c5b2c
jpaveiro <jpaveiros@gmail.com>
2026-01-07 03:04:17 -0300

    feat: top label and new section on app main

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,31 @@
-import Navbar from "@/src/components/navbar";
-import TopBar from "@/src/components/topbar";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { theme } from "@/theme";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Navbar from "../components/navbar";
 import RestaurantsFeed from "../components/restaurants-feed";
+import TopBar from "../components/topbar";
 
 export default function Index() {
     return (
         <View style={{ flex: 1 }}>
             <TopBar/>
-            <ScrollView>
-                <View style={styles.contentWrapper}>
-                    <RestaurantsFeed/>
+            <ScrollView style={styles.mainContentWrapper}>
+                <View style={styles.topContent}>
+                    <Text style={styles.topLabel}>Olá, João Pedro!</Text>
                 </View>
+
+                <Text style={styles.feedTitle}>Últimas lojas</Text>
+                <ScrollView style={styles.horizontalContentWrapper} horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <View>
+                        <RestaurantsFeed/>
+                    </View>
+                </ScrollView>
+
+                <Text style={styles.feedTitle}>Mais pedidos</Text>
+                <ScrollView style={styles.horizontalContentWrapper} horizontal={true} showsHorizontalScrollIndicator={false}>
+                    <View>
+                        <RestaurantsFeed/>
+                    </View>
+                </ScrollView>
             </ScrollView>
             <Navbar/>
         </View>
@@ -19,13 +34,32 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-    contentWrapper: {
-        flex: 1,
+    mainContentWrapper: {
         marginTop: 103, // Tamanho total do topbar
         marginBottom: 73, // Tamanho total da navbar
-        marginLeft: '5%',
-        marginRight: '5%',
+        marginLeft: '5%'
+    },
+
+    verticalContentWrapper: {
         borderWidth: 1,
         borderColor: 'red'
+    },
+
+    horizontalContentWrapper: {
+        borderWidth: 1,
+        borderColor: 'red'
+    },
+
+    topContent: {
+        marginBottom: 20
+    },
+
+    topLabel: {
+        fontSize: theme.fonts.size.large
+    },
+
+    feedTitle: {
+        marginTop: 20,
+        fontSize: theme.fonts.size.large 
     }
 });
